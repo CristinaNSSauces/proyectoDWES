af2e3ba6ef20ed48a8e29a9a1cb03a111a546c68
CristinaNS <cristina.nunseb@educa.jcyl.es>
2020-11-05 18:05:04 +0100

    add proyectoDWES

---
+++ indexProyectoDWES.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CNS-DWES</title>
+
+    <link href="webroot/css/estilo.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="logo">Desarrollo web en entorno servidor</div>
+        <nav>
+            <ul class="enlaces">
+                <li><a href="../index.html">HOME</a></li>
+                <li><a class="activo" href="indexProyectoDWES.php">DWES</a></li>
+                <li><a href="#">DWEC</a></li>
+                <li><a href="#">DAW</a></li>
+                <li><a href="#">DIW</a></li>
+                <li><a href="#">EIE</a></li> 
+            </ul>
+        </nav> 
+    </header>
+    
+    <main>
+        <div class="contenido"> 
+            <ul>
+                <li><a href="doc/EstudioTema1.pdf" target="_blank">TEMA 1: DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>
+                <li><a href="doc/EstudioTema2.pdf" target="_blank">TEMA 2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</a></li>
+                <li><a href="../proyectoTema3/indexProyectoTema3.php">TEMA 3: CARACTERÍSTICAS DEL LENGUAJE PHP</a></li>
+                <li><a href="../proyectoTema4/indexProyectoTema4.php">TEMA 4: TÉCNICAS DE ACCESO A DATOS</a></li>
+            </ul>
+        </div>
+    </main>
+    
+    <footer> 
+        <address>Cristina Núñez Sebastián</address>
+    </footer> 
+</body>
+</html>
